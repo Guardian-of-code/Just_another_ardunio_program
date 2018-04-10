979c02917356f7dfab4fd532e8e35a6a125e5175
Simhadri <simhadri064@gmail.com>
2018-04-10 21:11:53 +0530

    Update mok.py
    70% complete

--- mok.py
+++ mok.py
@@ -10,25 +10,24 @@ import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import Range
 
-d = {'L' : 'R','R' : 'L'}   # dictionary so that for min_turn it turns in opposite direction
-global cur_range  # making this global so any update anyware gets immediately reflected 
-global time_array
-global d_array
-count = 0 
+
 
 class MasterNode(object):
+    count = 0 
     def __init__(self):
         self.motor_pub = rospy.Publisher("motor_cmd", String, queue_size=5)
         self.ping_sub = rospy.Subscriber("ping_sensor", Range, self.callback) #continously calls cllback when data is recived
+        self.cur_range  # making this global so any update anyware gets immediately reflected 
+        self.time_array = []
+        self.d_array = []
         self.min_distance = 5
         self.start_time_count = 0
         self.aligndistance = 0
         print("Initiated Node")
 
     def callback(self, data):
-        global cur_range
-        cur_range = data.range
-        self.check_range(cur_range)
+        self.cur_range = data.range
+        self.check_range()               #Doubt will it execute every 500ms or wait till 2 secs
         # self.motor_pub.publish(cur_cmd)
         # rospy.loginfo("Bot is moving - {0}".format(cur_cmd))
 
@@ -38,39 +37,39 @@ class MasterNode(object):
         return
 
     def check_range(self):
-        global d_array  # direction array
-        d_array = []
-        global time_array
-        time_array = []
+         # direction array
+     
         motor_cmd = "F"
         
-        if cur_range < self.min_distance and count == 0:  # 1ST major turn # make this run only once***
+        if self.cur_range < self.min_distance and self.count == 0:  # 1ST major turn # make this run only once***
             motor_cmd = "R"
-            d_array.append("R")
-            global count
-            count = 1
+            self.d_array.append("R")
+            
+            self.count = 1
             self.publish_cmd(motor_cmd)  # publish
-            self.check_range_2(motor_cmd)
-            # motor_cmd = "F"                             #not needed i guess
+            self.check_range_2()
+            #motor_cmd = "F"                             #not needed i guess
             # self.motor_pub.publish()
             # rospy.loginfo("Bot is moving - {0}".format(motor_cmd))
-        elif count == 0 :
+        elif self.count == 0 :
             motor_cmd = "F"
             self.publish_cmd(motor_cmd)
-        if(motor_cmd == "R" or motor_cmd == 'L'):  # will be left in T
+       
+       
+        
+        elif(motor_cmd == "F" and self.count>=1):  # will be left in T
             self.check_range_2(motor_cmd)
         
         
-    def check_range_2(self, motor_cmd):
+    def check_range_2(self):
             self.start_time_count = time.time()
             motor_cmd = self.min_turn()    # call min_turn
             self.publish_cmd(motor_cmd)  # publish
+            while(motor_cmd!='F'):
+                motor_cmd=self.minturn()
+                self.publish_cmd(motor_cmd)
+            self.time_array.append(self.start_time_count - time.time())
 
-            if(motor_cmd == "F"):
-                time_array.append(self.start_time_count - time.time())
-
-            else:
-                # now to loop it
 
 
 
@@ -85,18 +84,18 @@ class MasterNode(object):
         start_time = time.time()
         while((time.time()-start_time) < max_time):     # to keep moving forward for fixed time
             self.motor_pub.publish('F')
-            rospy.loginfo("Bot is moving - {0}".format('F'))
-        global d_array 
+            rospy.loginfo("Bot is moving forward for 2 secs- {0}".format('F'))
+       
         motor_cmd = "L"
         self.publish_cmd(motor_cmd)  # publish
-        if(cur_range <= self.min_distance ):
+        if(self.cur_range <= self.min_distance ):
             return "R"             # revert back cause min distance> cur_range dictonary of opp
         else:
-            d_array.append(motor_cmd)  # add todirection array
+            self.d_array.append(motor_cmd)  # add todirection array
             return "F"                     # no obstacal move forward
             
         
-	
+    
 
 
 def main(args):
