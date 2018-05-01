f6a7ee69accae571f2c73182894b082a2c47dd4d
Simhadri <simhadri064@gmail.com>
2018-05-01 17:20:21 +0530

    Update moks_another_answer.py
    rewritten code

--- moks_another_answer.py
+++ moks_another_answer.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Team A
+"""
+
 import sys
 import rospy
 import time
@@ -10,21 +15,18 @@ class MasterNode(object):
         self.motor_pub = rospy.Publisher("motor_cmd", String, queue_size=5)
         self.ping_sub = rospy.Subscriber("ping_sensor", Range, self.callback)
         self.min_distance = 5
-        self.start_time = 0
-        
+        self.timearray = [0, 0, 0]
         self.turn_delay = 3.5
-        
-        self.max_time = 6
-        self.d_array = []
-        self.time_counter=0
-        self.time_array=[0,0,0]
-        self.temp_cmd = "Random"
+        self.max_time = 5
+        self.start_time = 0
+        self.temp_cmd = "not left"
+        self.start_time1 = 0
+        self.timecounter = 0
         print("Initiated Node")
 
     def callback(self, data):
         cur_range = data.range
         cur_cmd = self.check_range(cur_range)
-        
         if (cur_cmd == 'F'):
             self.motor_pub.publish(cur_cmd)
             rospy.loginfo("Bot is moving - {0}".format(cur_cmd))
@@ -34,62 +36,48 @@ class MasterNode(object):
             self.motor_pub.publish(cur_cmd)
             rospy.loginfo("Bot is moving - {0}".format(cur_cmd))
             time.sleep(self.turn_delay)
-            
 
     def check_range(self, distance):
         motor_cmd = "F"
+        if self.temp_cmd == "L" and distance >= self.min_distance:
+            t = time.time() - self.start_time1
+            if(t >= self.max_time()):
+                motor_cmd = "L"
+                self.timearray.append(self.timecounter)
+                self.temp_cmd == 'not Left'
+                if(self.timearray[0] != 0 and self.timearray[1] != 0):
+                    while(time.time-self.start_time1 <= self.timearray[0]):
+                        self.motor_pub.publish('F')
+                    motor_cmd = "R"
+                    self.reset_values()
 
-        if self.temp_cmd == "L":
-            motor_cmd = self.check1(distance)
-            if motor_cmd=="R":
-                self.time_counter+=6
-                self.start_time = time.time()
-            self.temp_cmd = "Random"
-
-        elif distance < self.min_distance:
-            self.start_time = (self.turn_delay+time.time())
+        elif distance <= self.min_distance:
+            self.start_time = (self.turn_delay + time.time())
             motor_cmd = "R"
-            self.d_array.append(motor_cmd)
 
-            # determine how long to turn 90deg                        
         else:
             motor_cmd = "F"
-            t = time.time()-self.start_time
-            if(t >=self.max_time() and self.start_time!=0): #self.starttime probably not needed
-                motor_cmd ="L"
-                self.time_counter=6
+            t = time.time() - self.start_time
+            if(t >= self.max_time() and self.start_time != 0): #self.starttime probably not needed	             if(t >=self.max_time() and self.start_time!=0): #self.starttime probably not needed
+                motor_cmd = "L"
+                self.time_counter += 12
                 self.temp_cmd = motor_cmd
-            if(self.time_array[0]<=(time.time()-self.start_time) and self.time_array[1]!=0):
-                self.start_time = 0
-                self.max_time = 6
-                self.d_array = []
-                self.time_counter=0
-                self.time_array=[0,0,0]
-                self.temp_cmd = "Random"
-                motor_cmd="R"
-            
-            
+                self.start_time1 = (self.turn_delay+time.time())
+                
         return motor_cmd
 
     def halt_bot(self):
         rospy.loginfo("Bot is coming to halt!")
         self.motor_pub.publish("X")
-    
-    def check1(self,distance):
-        #motor_cmd = "L"
-        #self.motor_pub.publish(motor_cmd)
-        #rospy.loginfo("Bot is moving - {0}".format(motor_cmd))  ## done
-        # publish
-        if(distance <= self.min_distance ):
-            return "R"             # revert back cause min distance> cur_range dictonary of opp
-        else:
-            self.d_array.append("L")  # add todirection array
-            self.time_array.append(self.time_counter)
-            self.start_time = time.time()
-            return "F"  
+    def reset_values(self):
+        self.timearray = [0, 0, 0]
+        self.turn_delay = 3.5
+        self.max_time = 5
+        self.start_time = 0
+        self.temp_cmd = "not left"
+        self.start_time1 = 0
+        self.timecounter = 0
         
-        
-
 def main(args):
     rospy.init_node('master_node', anonymous=False)
     mn = MasterNode()
@@ -102,5 +90,4 @@ def main(args):
 
 if __name__ == '__main__':
     main(sys.argv)
-
 
